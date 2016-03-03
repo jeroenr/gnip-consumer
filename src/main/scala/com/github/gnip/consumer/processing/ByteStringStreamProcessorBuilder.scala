64a79c7dabddb10863d62305937f4869b35ff245
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2016-03-03 16:24:58 +0100

    Prepare for Scala Academy

--- src/main/scala/com/github/gnip/consumer/processing/ByteStringStreamProcessorBuilder.scala
+++ src/main/scala/com/github/gnip/consumer/processing/ByteStringStreamProcessorBuilder.scala
@@ -32,29 +32,9 @@ class ByteStringStreamProcessorBuilder(config: Config)(implicit val materializer
       GraphDSL.create(src) { implicit builder =>
         byteStringSource =>
           import GraphDSL.Implicits._
-
-          // create broadcaster with as many outlets as we have processors
-          val tweetBroadCaster = builder.add(Broadcast[Tweet](2))
-
-          val byteStringToTweetFlow = Flow[ByteString]
-            .map(byteString => byteString.utf8String) // decode bytestring to string
-            .map(unzipped => Try(JsonUtil.fromJson(unzipped))) // try parse string to Map[String, Any]
-            .collect { case Success(json) => json } // filter unsuccessfully parsed items
-            .filter(_.contains("actor")) // filter tweets (drop system messages)
-
-          val tweetToBulk = Flow[Tweet]
-            .map(JsonUtil.toBson) // transform tweet to Bson
-            .groupedWithin(100, 10 millis) // try to group up to 100 BsonDocuments in 10 milliseconds
-            .map(_.toList)
-            .map(Bulk) // create a Bulk insert operation
-
-          val mongoDbSink = mongoCollection.sink(parallelism = 4)
-          val tweetPrintSink = Sink.foreach[Tweet](println)
-
-          // route bytestring source via bytestring-to-tweet transformer to broadcaster
-          byteStringSource.out ~> Gzip.decoderFlow ~> byteStringToTweetFlow ~> tweetBroadCaster.in
-          tweetBroadCaster.out(0) ~> tweetToBulk ~> mongoDbSink
-          tweetBroadCaster.out(1) ~> tweetPrintSink
+          // TODO: 1. filter out tweets from the stream (we are not interested in keep alive / system messages etc)
+          // TODO: 2. group them in bulks
+          // TODO: 3. index bulk of tweets to Mongo
           ClosedShape
       }
     }
